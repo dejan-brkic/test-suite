cd5119d1ead4c8ac0071fa1b85d93223d7923462
luishernandezmonge <luis.hernandez@craftersoftware.com>
2018-02-02 14:52:13 -0600

    -Added new test case: ID: 1 of Workbook: Studio - Contextual Navigation     Test Name: Verify that the application shows the account management page when user clicks on settings contextual navigation bar option

---
+++ src/test/java/org/craftercms/studio/test/cases/accountmanagementtestcases/ShowAccountManagementTest.java
@@ -0,0 +1,55 @@
+package org.craftercms.studio.test.cases.accountmanagementtestcases;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.craftercms.studio.test.cases.BaseTest;
+import org.craftercms.studio.test.utils.APIConnectionManager;
+
+public class ShowAccountManagementTest extends BaseTest {
+
+	private String userName;
+	private String password;
+	private APIConnectionManager apiConnectionManager;
+	private String accountManagementTitle;
+	final static Logger logger = LogManager.getLogger(ShowAccountManagementTest.class);
+
+	@BeforeMethod
+	public void beforeTest() {
+		userName = constantsPropertiesManager.getSharedExecutionConstants().getProperty("crafter.username");
+		password = constantsPropertiesManager.getSharedExecutionConstants().getProperty("crafter.password");
+		apiConnectionManager = new APIConnectionManager();
+		accountManagementTitle = uiElementsPropertiesManager.getSharedUIElementsLocators()
+				.getProperty("accountManagement.pageTitle");
+	}
+
+	@Test(priority = 0)
+	public void VerifyThatApplicationShowsAccountManagementPageWhenUserClicksSettingsContextualNavigationOption() {
+
+		// login to application
+		logger.info("Login into Crafter");
+		loginPage.loginToCrafter(userName, password);
+
+		// wait for login page to close
+		driverManager.waitUntilLoginCloses();
+
+		// wait for element is clickable
+		createSitePage.clickAdmin();
+
+		// click on settings
+		createSitePage.clickOnSettingsOption();
+
+		this.driverManager.waitForAnimation();
+		this.driverManager.waitUntilElementIsDisplayed("xpath", accountManagementTitle);
+		
+		// Checking if the Sites page was Loaded
+		Assert.assertTrue(accountManagementPage.getDriverManager().getDriver().getCurrentUrl()
+				.equals(apiConnectionManager.getHeaderLocationBase() + "/studio/#/settings"));
+
+		// Checking if the Users title is displayed on the current page
+		Assert.assertTrue(accountManagementPage.isAccountManagementTitlePresent());
+
+	}
+}
