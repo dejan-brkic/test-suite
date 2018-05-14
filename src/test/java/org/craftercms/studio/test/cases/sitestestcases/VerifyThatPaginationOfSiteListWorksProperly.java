3fbef6ec78ead5a4e17e1241dac0be30a225cd0f
LuisHernandez <luis.hernandez@craftersoftware.com>
2018-05-14 12:54:14 -0600

    Added new test case to cover ticket #2127. Also added to the testng.xml test suite

--- src/test/java/org/craftercms/studio/test/cases/sitestestcases/VerifyThatPaginationOfSiteListWorksProperly.java
+++ src/test/java/org/craftercms/studio/test/cases/sitestestcases/VerifyThatPaginationOfSiteListWorksProperly.java
@@ -21,15 +21,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.craftercms.studio.test.cases.StudioBaseTest;
-
-
 /**
  * 
- * @author Gustavo Andrei Ortiz Alfaro
+ * @author luishernandez
  *
  */
 
-public class PaginationOfListOfSitesTest extends StudioBaseTest{
+public class VerifyThatPaginationOfSiteListWorksProperly extends StudioBaseTest{
 	
 	private String userName;
 	private String password;
@@ -156,7 +154,7 @@ public class PaginationOfListOfSitesTest extends StudioBaseTest{
 	}
 
 	@Test(priority = 0)
-	public void paginationOfTheListOfSites() {
+	public void verifyThatThePaginationOnSitesListWorkProperly() {
 		navigationOfPage();
 	}
 
